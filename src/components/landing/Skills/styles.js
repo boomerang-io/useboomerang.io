6f12ea59a4b5c00f9980b53c85586394469cbf06
Timothy R. Bula <trbula@us.ibm.com>
2020-06-17 14:50:58 -0400

    feat: update flow section

--- src/components/landing/Skills/styles.js
+++ src/components/landing/Skills/styles.js
@@ -1,84 +1,88 @@
 import styled from 'styled-components'
 
 export const Wrapper = styled.section`
-	//background-image: url('../illustrations/details.svg');
-	background-size: contain;
-	background-position: left top;
-	background-repeat: no-repeat;
+  //background-image: url('../illustrations/details.svg');
+  background-size: contain;
+  background-position: left top;
+  background-repeat: no-repeat;
 `
 
 export const SkillsWrapper = styled.div`
-	padding: 4rem 0;
-	display: flex;
-	align-items: center;
-	justify-content: space-between;
-	flex-direction: ${props => (props.reverse ? 'row-reverse' : 'row')};
-	@media (max-width: 960px) {
-		flex-direction: column;
-	}
+  padding: 4rem 0;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  flex-direction: ${props => (props.reverse ? 'row-reverse' : 'row')};
+  @media (max-width: 960px) {
+    flex-direction: column;
+  }
 `
 
 export const Details = styled.section`
-	flex: 1;
+  flex: 1;
 
-	@media (max-width: 960px) {
-		padding-left: unset;
-		width: 100%;
-	}
+  @media (max-width: 960px) {
+    padding-left: unset;
+    width: 100%;
+  }
 
-	h1 {
-		margin-bottom: 1rem;
-		font-size: 26pt;
-		color: #212121;
-	}
+  h1 {
+    margin-bottom: 1rem;
+    font-size: 26pt;
+    color: #212121;
+  }
 
-	p {
-		margin-bottom: 2rem;
-		font-size: 14pt;
-		font-weight: 300;
-		line-height: 1.1;
-		color: #212121;
-	}
+  p {
+    margin-bottom: 2rem;
+    font-size: 14pt;
+    font-weight: 300;
+    line-height: 1.1;
+    color: #212121;
+  }
 
-	ul {
-		margin-bottom: 2rem;
-	}
+  ul {
+    margin-bottom: 2rem;
+  }
 
-	li {
-		font-size: 14pt;
-		font-weight: 300;
-		line-height: 1.1;
-	}
+  li {
+    font-size: 14pt;
+    font-weight: 300;
+    line-height: 1.1;
+  }
 `
 
 export const Thumbnail = styled.div`
-	flex: 1;
+  flex: 1;
 
-	@media (max-width: 960px) {
-		width: 100%;
-		margin-bottom: 2rem;
-	}
+  @media (max-width: 960px) {
+    width: 100%;
+    margin-bottom: 2rem;
+  }
 `
 
 export const ButtonLinkSection = styled.section`
-	display: flex;
-	align-items: center;
+  display: flex;
+  align-items: center;
 `
 
 export const ButtonLink = styled.a`
-	display: flex;
-	justify-content: center;
-	align-items: center;
-	background: #0072c3;
-	color: white;
-	padding: 0.375rem;
-	border-radius: 0.25rem;
-	width: 8rem;
-	height: 2rem;
-	font-size: 1rem;
-	letter-spacing: 0.0125rem;
-	margin-right: 1rem;
-	&:hover {
-		background: #0058a1;
-	}
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: #0072c3;
+  color: white;
+  padding: 0.375rem;
+  border-radius: 0.25rem;
+  width: 8rem;
+  height: 2rem;
+  font-size: 1rem;
+  letter-spacing: 0.0125rem;
+  margin-right: 1rem;
+  transition: all 0.3s ease;
+  &:active,
+  &:hover,
+  &:focus {
+    background: #0058a1;
+    transform: scale(1.02);
+  }
 `
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,49 +5,57 @@ import bosun from 'Static/illustrations/bosun.svg'
 import comingSoon from 'Static/illustrations/coming-soon.svg'
 
 export default () => (
-	<Layout>
-		<SEO />
-		<Intro />
-		<Skills
-			title="Bosun"
-			paragraph={(
-    <>
-					Policy-based gating system that combines Policy Definitions with Rules
-					and Metrics Data to validate if specific Gates are passed.
-					<div style={{ paddingBottom: '1rem' }} />
-					Bosun provides a UI to interact with and manage your policies and
-					validation activities. It leverages{' '}
-					<a href="https://www.openpolicyagent.org/">
-						Open Policy Agent (OPA){' '}
-					</a>{' '}
-					at its core.
-				</>
-			)}
-			imgAlt="Bosun"
-			imgSrc={bosun}
-			listItems={['CI/CD Gates', 'Generic policy validation']}
-			linkButtons={[
-				{
-					text: 'Get Started',
-					link:
-						'https://github.com/boomerang-io/boomerang.docs/blob/stable/content/bosun.md',
-				},
-				{
-					text: 'Try Me',
-					link: 'https://try.useboomerang.io/public/bosun/web/bosun',
-				},
-			]}
-		/>
-		<Skills
-			reverse
-			title="Flow"
-			paragraph="Cloud-native workflow automation. Coming soon."
-			imgAlt="Flow"
-			imgSrc={comingSoon}
-		/>
-		<Projects />
-		{
-			// <Contact />
-		}
-	</Layout>
+  <Layout>
+    <SEO />
+    <Intro />
+    <Skills
+      title="Bosun"
+      paragraph={
+        <>
+          Policy-based gating system that combines Policy Definitions with Rules
+          and Metrics Data to validate if specific Gates are passed.
+          <div style={{ paddingBottom: '1rem' }} />
+          Bosun provides a UI to interact with and manage your policies and
+          validation activities. It leverages{' '}
+          <a href="https://www.openpolicyagent.org/">
+            Open Policy Agent (OPA){' '}
+          </a>{' '}
+          at its core.
+        </>
+      }
+      imgAlt="Bosun"
+      imgSrc={bosun}
+      listItems={['CI/CD Gates', 'Generic policy validation']}
+      linkButtons={[
+        {
+          text: 'Get Started',
+          link:
+            'https://github.com/boomerang-io/boomerang.docs/blob/stable/content/bosun.md',
+        },
+        {
+          text: 'Try Me',
+          link: 'https://try.useboomerang.io/public/bosun/web/bosun',
+        },
+      ]}
+    />
+    <Skills
+      reverse
+      title="Flow"
+      paragraph="Cloud-native workflow automation. Work in Progress."
+      imgAlt="Flow"
+      imgSrc={comingSoon}
+      listItems={['"No-code" workflow creation']}
+      linkButtons={[
+        {
+          text: 'Get Started',
+          link:
+            'https://github.com/boomerang-io/boomerang.docs/blob/stable/content/flow.md',
+        },
+      ]}
+    />
+    <Projects />
+    {
+      // <Contact />
+    }
+  </Layout>
 )
