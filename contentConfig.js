0bed7ae53d6de2a9193863a960f19317d6c4294f
Ben Ruby <benjamin.ruby@ibm.com>
2021-04-13 19:09:41 -0600

    chore: adding images for bosun so no break

--- contentConfig.js
+++ contentConfig.js
@@ -21,36 +21,37 @@ const config = {
       title: 'Discover Boomerang Flow',
       links: [
         {
-          title: "Introduction",
+          title: 'Introduction',
           description:
-            "Get to know Boomerang Flow, an event driven cloud-native workflow orchestration engine.",
-          path: "/docs/boomerang-flow/introduction/overview",
+            'Get to know Boomerang Flow, an event driven cloud-native workflow orchestration engine.',
+          path: '/docs/boomerang-flow/introduction/overview',
         },
         {
-          title: "Architecture",
+          title: 'Architecture',
           description:
-            "Provides the architecture, the patterns, technologies, and implementation.",
-          path: "/docs/boomerang-flow/architecture/overview",
+            'Provides the architecture, the patterns, technologies, and implementation.',
+          path: '/docs/boomerang-flow/architecture/overview',
         },
         {
-          title: "Getting to Know",
-          description: "Familiarize yourself with the functionality available",
-          path: "/docs/boomerang-flow/getting-to-know/workflows",
+          title: 'Getting to Know',
+          description: 'Familiarize yourself with the functionality available',
+          path: '/docs/boomerang-flow/getting-to-know/workflows',
         },
         {
-          title: "How to Guide",
-          description: "Deep dive into how to perform different functions.",
-          path: "/docs/boomerang-flow/how-to-guide/managing-workflows",
+          title: 'How to Guide',
+          description: 'Deep dive into how to perform different functions.',
+          path: '/docs/boomerang-flow/how-to-guide/managing-workflows',
         },
         {
-          title: "Installing",
-          description: "Learn what it takes, and how, to install your own instance.",
-          path: "/docs/boomerang-flow/installing/installing",
+          title: 'Installing',
+          description:
+            'Learn what it takes, and how, to install your own instance.',
+          path: '/docs/boomerang-flow/installing/installing',
         },
         {
-          title: "Tutorials",
-          description: "Explore practical examples on how to implement.",
-          path: "/docs/boomerang-flow/tutorials/github-issues-bot",
+          title: 'Tutorials',
+          description: 'Explore practical examples on how to implement.',
+          path: '/docs/boomerang-flow/tutorials/github-issues-bot',
         },
       ],
     },
@@ -58,16 +59,18 @@ const config = {
       title: 'Discover Boomerang Bosun',
       links: [
         {
-          title: "Introduction",
+          title: 'Introduction',
           description:
-            "Get to know Boomerang Bosun, a policy-based continuous delivery gating system.",
-          path: "/docs/boomerang-bosun/introduction/overview",
+            'Get to know Boomerang Bosun, a policy-based continuous delivery gating system.',
+          path: '/docs/boomerang-bosun/introduction/overview',
+          image: 'automation-control-center',
         },
         {
-          title: "Architecture",
+          title: 'Architecture',
           description:
-            "Provides the architecture, the patterns, technologies, and implementation.",
-          path: "/docs/boomerang-bosun/architecture/overview",
+            'Provides the architecture, the patterns, technologies, and implementation.',
+          path: '/docs/boomerang-bosun/architecture/overview',
+          image: 'process-delivery-accelerator',
         },
       ],
     },
