8d6ff87b21e516aa00e240f8bf406efbedd46d9d
Timothy R. Bula <trbula@us.ibm.com>
2021-07-28 12:22:46 -0500

    fix: add doc title, some prettier formatting

--- src/components/flow/FlowInfoSection/index.jsx
+++ src/components/flow/FlowInfoSection/index.jsx
@@ -1,42 +1,64 @@
-import React from 'react';
-import cx from "classnames";
+import React from 'react'
+import cx from 'classnames'
 import SectionTransition from '../SectionTransition'
-import { Loading } from "@boomerang-io/carbon-addons-boomerang-react"
-import * as styles from './FlowInfoSection.module.scss';
+import { Loading } from '@boomerang-io/carbon-addons-boomerang-react'
+import * as styles from './FlowInfoSection.module.scss'
 
-export const FlowInfoSection = ({ descriptionArray = [], id, imageSrc, subtitle, title, transition = true, theme = "dark", videoSrc }) => {
-  const [videoIsLoading, setVideoIsLoading] = React.useState(true);
-  const isLightTheme = theme === "light";
+export const FlowInfoSection = ({
+  descriptionArray = [],
+  id,
+  imageSrc,
+  subtitle,
+  title,
+  transition = true,
+  theme = 'dark',
+  videoSrc,
+}) => {
+  const [videoIsLoading, setVideoIsLoading] = React.useState(true)
+  const isLightTheme = theme === 'light'
 
   const TextContent = () => {
     return (
       <>
         <p className={cx(styles.contentTitle, styles[theme])}>{title}</p>
-        {subtitle && <p className={cx(styles.contentSubtitle, styles[theme])}>{subtitle}</p>}
+        {subtitle && (
+          <p className={cx(styles.contentSubtitle, styles[theme])}>
+            {subtitle}
+          </p>
+        )}
         <ul className={cx(styles.contentDescritionList, styles[theme])}>
-          {descriptionArray.map((description, index) => <li key={index} className={styles.contentDescription}>{description}</li>)}
+          {descriptionArray.map((description, index) => (
+            <li key={index} className={styles.contentDescription}>
+              {description}
+            </li>
+          ))}
         </ul>
       </>
     )
   }
 
   const VideoImageContent = () => {
-    if(imageSrc) return <img className={styles.contentImage} src={imageSrc}/>;
-    if(videoSrc) return (
-      <div className={styles.videoContainer}>
-        {videoIsLoading && <div className={styles.videoLoading}><Loading withOverlay={false} /></div>}
-        <iframe
-          allowFullScreen
-          className={styles.youtubeVideo}
-          frameborder="0"
-          loading="lazy"
-          onLoad={() => setVideoIsLoading(false)}
-          src={videoSrc}
-        />
-      </div>
-    );
+    if (imageSrc) return <img className={styles.contentImage} src={imageSrc} />
+    if (videoSrc)
+      return (
+        <div className={styles.videoContainer}>
+          {videoIsLoading && (
+            <div className={styles.videoLoading}>
+              <Loading withOverlay={false} />
+            </div>
+          )}
+          <iframe
+            allowFullScreen
+            className={styles.youtubeVideo}
+            frameborder="0"
+            loading="lazy"
+            onLoad={() => setVideoIsLoading(false)}
+            src={videoSrc}
+          />
+        </div>
+      )
 
-    return null;
+    return null
   }
   return (
     <>
@@ -46,13 +68,21 @@ export const FlowInfoSection = ({ descriptionArray = [], id, imageSrc, subtitle,
             <div className={styles.leftContent}>
               <TextContent />
             </div>
-            <div className={cx(styles.rightContent, { [styles.isVideo]: Boolean(videoSrc) })}>
+            <div
+              className={cx(styles.rightContent, {
+                [styles.isVideo]: Boolean(videoSrc),
+              })}
+            >
               <VideoImageContent />
             </div>
           </div>
         ) : (
           <div className={styles.content}>
-            <div className={cx(styles.leftContent, { [styles.isVideo]: Boolean(videoSrc) })}>
+            <div
+              className={cx(styles.leftContent, {
+                [styles.isVideo]: Boolean(videoSrc),
+              })}
+            >
               <VideoImageContent />
             </div>
             <div className={styles.rightContent}>
--- src/components/flow/Intro/index.jsx
+++ src/components/flow/Intro/index.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import SectionTransition from '../SectionTransition'
-import Link from "@boomerang-io/gatsby-theme-boomerang/src/components/Link"
-import { Button, Loading } from "@boomerang-io/carbon-addons-boomerang-react"
+import Link from '@boomerang-io/gatsby-theme-boomerang/src/components/Link'
+import { Button, Loading } from '@boomerang-io/carbon-addons-boomerang-react'
 import FlowLogo from '../../graphics/FlowLogo'
 import ArrowRight16 from '@carbon/icons-react/es/arrow--right/16'
 import * as styles from './Intro.module.scss'
 
 export const Intro = () => {
-  const [videoIsLoading, setVideoIsLoading] = React.useState(true);
+  const [videoIsLoading, setVideoIsLoading] = React.useState(true)
 
   return (
     <div className={styles.container}>
@@ -15,15 +15,16 @@ export const Intro = () => {
       <div className={styles.content}>
         <div className={styles.leftContent}>
           <div className={styles.flow}>
-            <FlowLogo className={styles.flowLogo}/>
+            <FlowLogo className={styles.flowLogo} />
             <p className={styles.flowText}>Boomerang Flow</p>
           </div>
           <p className={styles.flowTitle}>
             Cloud-native orchestration tool for workflow automation
           </p>
           <p className={styles.flowDescription}>
-            A new scalable and extensible modern approach to automation focused on
-            the end user experience with lower cost and increased productivity
+            A new scalable and extensible modern approach to automation focused
+            on the end user experience with lower cost and increased
+            productivity
           </p>
           <Link to={'/docs/boomerang-flow/introduction/getting-started'}>
             <Button renderIcon={ArrowRight16}>Get Started</Button>
@@ -31,8 +32,12 @@ export const Intro = () => {
         </div>
         <div className={styles.rightContent}>
           <div className={styles.videoContainer}>
-            {videoIsLoading && <div className={styles.videoLoading}><Loading withOverlay={false} /></div>}
-            <iframe 
+            {videoIsLoading && (
+              <div className={styles.videoLoading}>
+                <Loading withOverlay={false} />
+              </div>
+            )}
+            <iframe
               allowFullScreen
               className={styles.youtubeVideo}
               frameborder="0"
